d2fc91917a3de17e5a85f79351d7c637284ce3c8
Eric D. Schabell <eric@schabell.org>
2013-04-16 11:06:37 +0200

    Renamed integration and unit tests.

--- projects/brms-rewards-demo/src/test/java/org/jbpm/rewards/RewardsApprovalTest.java
+++ projects/brms-rewards-demo/src/test/java/org/jbpm/rewards/RewardsApprovalTest.java
@@ -1,107 +1,148 @@
 package org.jbpm.rewards;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
 import org.drools.KnowledgeBase;
-import org.drools.SystemEventListenerFactory;
 import org.drools.builder.ResourceType;
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.process.ProcessInstance;
 import org.jbpm.process.instance.impl.demo.SystemOutWorkItemHandler;
-import org.jbpm.task.service.TaskClient;
-import org.jbpm.task.service.hornetq.CommandBasedHornetQWSHumanTaskHandler;
-import org.jbpm.task.service.hornetq.HornetQTaskClientConnector;
-import org.jbpm.task.service.hornetq.HornetQTaskClientHandler;
+import org.jbpm.task.AccessType;
+import org.jbpm.task.Status;
+import org.jbpm.task.query.TaskSummary;
+import org.jbpm.task.service.ContentData;
+import org.jbpm.task.TaskService;
+import org.jbpm.task.service.responsehandlers.BlockingTaskSummaryResponseHandler;
 import org.jbpm.test.JbpmJUnitTestCase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+
+
 /**
- * This is a sample file to integration test the Extended Rewards Approval process.
- * 
- * Note: you must have BRMS server running as this process test 
- *       uses a running HornetQ messaging system for its 
- *       integration test.
- */
+* This is a sample file to unit test the Extended Rewards Approval process.
+*/
+
 public class RewardsApprovalTest extends JbpmJUnitTestCase {
 
-	TaskClient client;
+	private static StatefulKnowledgeSession ksession;
+	private static TaskService taskService;
+	private static Map<String, Object> params;
+	private static ProcessInstance processInstance;
 
-    @BeforeClass
-    public static void setUpOnce() throws Exception {
-    	// nothing yet.
-    }
+	public RewardsApprovalTest() {
 
-    @Override
-    @Before
-    public void setUp() throws Exception {
-        super.setUp();
+		super(true);
 
-		client = new TaskClient(new HornetQTaskClientConnector("taskClient" + UUID.randomUUID(), 
-					new HornetQTaskClientHandler(SystemEventListenerFactory.getSystemEventListener())));
-		client.connect("127.0.0.1", 5153);
-    }
+	}
 
-    @Override
-    @After
-    public void tearDown() throws Exception {
-    	client.disconnect();
-    	super.tearDown();
-    }
+	@BeforeClass
+	public static void setUpOnce() throws Exception {
+		// nothing yet.
+	}
 
-    @Test
-    public void submitEmployee() {
- 	  	// load up the knowledge base
- 	  	KnowledgeBase kbase = null;
- 	  	
-      // Use the local files.
-      final Map<String, ResourceType> resources = new HashMap<String, ResourceType>();
-      resources.put("rewardsapproval.bpmn2", ResourceType.BPMN2);
-      
-			try {
- 	  		kbase = createKnowledgeBase(resources);
- 	  	} catch (Exception e) {
- 	  		// TODO Auto-generated catch block
- 	  		e.printStackTrace();
- 	  	}
- 
- 	  	StatefulKnowledgeSession ksession = createKnowledgeSession(kbase); 
- 
- 	  	// setup task client to use running BRMS server task client.
- 	  	CommandBasedHornetQWSHumanTaskHandler handler = new CommandBasedHornetQWSHumanTaskHandler(ksession);
- 	  	handler.setClient(client);
- 
- 	  	// register work items.
- 	  	ksession.getWorkItemManager().registerWorkItemHandler("Log", new SystemOutWorkItemHandler());
- 	  	ksession.getWorkItemManager().registerWorkItemHandler("Email", new SystemOutWorkItemHandler());
- 	  	ksession.getWorkItemManager().registerWorkItemHandler("Human Task", handler);
-       	
-       	// setup our input request for processing.
- 	  	final Map<String, Object> params = new HashMap<String, Object>();
- 	  	params.put("employee", "erics");
- 	  	params.put("reason", "Amazing demos for JBoss World");
- 	  	
- 	  	System.out.println("=================================================");
- 	  	System.out.println("=    Unit Test: Submit Employee Test Case       =");
- 	  	System.out.println("=================================================");
- 	  	
- 	  	// start a new process instance        
- 	  	final ProcessInstance processInstance = ksession.startProcess("org.jbpm.approval.rewards", params);
- 
- 	  	// Check whether the process instance has completed successfully.
- 	  	assertProcessInstanceActive(processInstance.getId(), ksession);
- 	  	System.out.println("Process is active.");
- 	  	assertNodeExists(processInstance, "Approve Reward");
- 	  	System.out.println("Process had node we are looking for.");
- 	  	assertNodeTriggered(processInstance.getId(), "Start", "Approve Reward");
- 	  	System.out.println("Process has triggerd node we expect it to do.");
- 	  	
- 	  	System.out.println("Finished test.");
-    }
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		
+
+		// load up the knowledge base
+		KnowledgeBase kbase = null;
+	
+		// Use the local files.
+	    final Map<String, ResourceType> resources = new HashMap<String, ResourceType>();
+	    resources.put("rewardsapproval.bpmn2", ResourceType.BPMN2);
+
+	    try {
+	    	kbase = createKnowledgeBase(resources);
+	 	} catch (Exception e) {
+	 		e.printStackTrace();
+	 	}
+
+	    ksession = createKnowledgeSession(kbase);
+		taskService = getTaskService(ksession);
+
+		// register work items.
+		ksession.getWorkItemManager().registerWorkItemHandler("Log", new SystemOutWorkItemHandler());
+		ksession.getWorkItemManager().registerWorkItemHandler("Email", new SystemOutWorkItemHandler());
+
+		params = new HashMap<String, Object>();
+		// initialize process parameters.
+		params.put("employee", "erics");
+		params.put("reason", "Amazing demos for JBoss World!");		
+	}
+
+	@Override
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	
+	@Test
+	public void rewardApprovedTest() {
+
+		// start process.
+		processInstance = ksession.startProcess("org.jbpm.approval.rewards", params);
+
+		// execute task by Mary from HR.		
+		List<TaskSummary> list = taskService.getTasksAssignedAsPotentialOwner("mary", new ArrayList<String>(), "en-UK");  // NP error here.
+		TaskSummary task = list.get(0);
+		taskService.claim(task.getId(), "mary", new ArrayList<String>());
+		taskService.start(task.getId(), "mary");
+		
+		Map<String, Object> taskParams = new HashMap<String, Object>();
+		taskParams.put("explanation", "Great work");
+		taskParams.put("outcome", "Approved");
+		
+		// Serialized and inserted.
+		ContentData content = new ContentData();
+		content.setAccessType(AccessType.Inline);
+		content.setContent(getByteArrayFromObject(taskParams));
+		
+		// add results of task.
+		taskService.complete(task.getId(), "mary", content);
+
+		// test for completion and in correct end node.
+		assertProcessInstanceCompleted(processInstance.getId(), ksession);
+		assertNodeTriggered(processInstance.getId(), "End Approved");
+	}
+
+	/**
+	 * Converts an object to a serialized byte array.
+	 * 
+	 * @param obj Object to be converted.
+	 * @return byte[] Serialized array representing the object.
+	 */
+	public static byte[] getByteArrayFromObject(Object obj) {
+	    byte[] result = null;
+	       
+	    try {
+	        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	        ObjectOutputStream oos = new ObjectOutputStream(baos);
+	        oos.writeObject(obj);
+	        oos.flush();
+	        oos.close();
+	        baos.close();
+	        result = baos.toByteArray();
+	    } catch (IOException ioEx) {
+	        Logger.getLogger("UtilityMethods").error("Error converting object to byteArray", ioEx);
+	    }
+	        
+	    return result;
+	}
 
 }
 
